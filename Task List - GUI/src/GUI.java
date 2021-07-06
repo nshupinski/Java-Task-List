96885086d9e385354d2921c207f6f12b7f1ee07b
nshupinski <55292866+nshupinski@users.noreply.github.com>
2021-07-06 12:53:08 -0400

    nothin much

--- Task List - GUI/src/GUI.java
+++ Task List - GUI/src/GUI.java
@@ -1,5 +1,8 @@
 import java.awt.BorderLayout;
 import java.awt.Button;
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.*;
 import java.util.ArrayList;
@@ -17,6 +20,7 @@ public class GUI implements ActionListener{
 	JFrame frame;
 	JPanel buttonPanel = new JPanel();
 	JPanel listPanel = new JPanel();
+	JPanel titlePanel = new JPanel();
 	
 	public GUI() {
 		
@@ -24,36 +28,41 @@ public class GUI implements ActionListener{
 		frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setTitle("To-Do List");
+		frame.setSize(350, 500);
 		frame.setLayout(new BorderLayout());
 		
-		//Declare
-		JButton button = new JButton("Click Me");
+		//Buttons
+		JButton button = new JButton("Add");
 		button.addActionListener(this);
+		button.setSize(10, 20);
+		buttonPanel.add(button);
 		
 		//Panel Details
+		titlePanel.setLayout(new FlowLayout());
 		buttonPanel.setLayout(new GridLayout(3,1));
-		
 		listPanel.setLayout(new GridLayout(3,1));
 		
+		titlePanel.setBackground(Color.GRAY);
+		buttonPanel.setBackground(Color.LIGHT_GRAY);
+		
 		//panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-		//panel.add(button);
-				
+		
+		frame.add(titlePanel, BorderLayout.NORTH);
 		frame.add(buttonPanel, BorderLayout.EAST);
 		frame.add(listPanel, BorderLayout.WEST);
 		
-		//frame.setSize(400, 500);
-		
 		//Labels
 		JLabel header = new JLabel("To-Do List");
-		header.setBounds(10, 20, 80, 25);
-		buttonPanel.add(header);
+		//header.setBounds(10, 20, 80, 25);
+		header.setFont(new Font("Serif", Font.BOLD, 20));
+		titlePanel.add(header);
 		
 		JTextField usertext = new JTextField(20);
 		usertext.setBounds(100, 20, 165, 25);
-		buttonPanel.add(usertext);
+		//buttonPanel.add(usertext);
 		
 		
-		frame.pack();
+		//frame.pack();
 		frame.setVisible(true);
 	}
 
@@ -66,9 +75,11 @@ public class GUI implements ActionListener{
 		for (String task : Tasks) {
 			JCheckBox chk = new JCheckBox(task.toString());
 		    chk.setSelected(false);
-		    chk.setBounds(10, 80, 80, 25);
-		    this.listPanel.add(chk);
+		    chk.setFont(new Font("Cobalt", Font.PLAIN, 16));
+		    //chk.setBounds(10, 80, 80, 25);
+		    listPanel.add(chk);
 		}
+		listPanel.revalidate();
 	}
 
 	@Override
